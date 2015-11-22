7527e6a0898613950049772f7cb2e118191c31bc
Kole <Kswearingen94@yahoo.com>
2015-11-22 01:34:24 +0000

    Finish comment structure

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+    has_many :comments, dependent: :destroy
+    
     validates :title, presence: true, length: { maximum: 30 }
     
     validates :text, presence: true
